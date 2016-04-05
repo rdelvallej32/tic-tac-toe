22cbb00585881d7c918f0aaf5d772f0c9c3c2f48
rdelvallej32 <rdelvallej@gmail.com>
2016-04-05 14:58:16 -0400

    Fix click event bug
    Fixed bug that overwrote a user's move.

--- assets/scripts/game/move.js
+++ assets/scripts/game/move.js
@@ -7,13 +7,13 @@ const playerMove = function() {
 
   $('.tile').on('click', function(event) {
     event.preventDefault();
-    if(currentPlayer === 'x') {
+    if(currentPlayer === 'x' && $(this).text() !== 'O') {
      $(this).text('X').css('font-size', '30px').css('text-align', 'center');
      currentPlayer = 'o';
      turn+=1;
    }
 
-     else {
+     else if (currentPlayer === 'o' && $(this).text() !== 'X') {
        $(this).text('O').css('font-size', '30px').css('text-align', 'center');
        currentPlayer = 'x';
        turn+=1;
