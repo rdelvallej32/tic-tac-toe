04df1e58c1a05430f0293721c9906a75e803c5a3
rdelvallej32 <rdelvallej@gmail.com>
2016-04-05 15:02:29 -0400

    Add class event handler
    Added class methods within playerMove to use as data for gameboard array.

--- assets/scripts/game/move.js
+++ assets/scripts/game/move.js
@@ -9,12 +9,14 @@ const playerMove = function() {
     event.preventDefault();
     if(currentPlayer === 'x' && $(this).text() !== 'O') {
      $(this).text('X').css('font-size', '30px').css('text-align', 'center');
+     $(this).removeClass('player2').addClass('player1');
      currentPlayer = 'o';
      turn+=1;
    }
 
      else if (currentPlayer === 'o' && $(this).text() !== 'X') {
        $(this).text('O').css('font-size', '30px').css('text-align', 'center');
+       $(this).removeClass('player1').addClass('player2');
        currentPlayer = 'x';
        turn+=1;
      }
