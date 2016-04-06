40ccaecf82ed8de873d71cdbbe9a44069510b03c
rdelvallej32 <rdelvallej@gmail.com>
2016-04-05 21:27:30 -0400

    Add win conditions

--- assets/scripts/game/gameplay.js
+++ assets/scripts/game/gameplay.js
@@ -2,13 +2,13 @@
 
 const game = require('./gameplay');
 
-// let topRow =
-// let middleRow =
-// let bottomRow =
-// let firstCol =
-// let secondCol =
-// let thirdCol =
-// let leftDiag =
+let topRow = [$('#tile1'), $('#tile2'), $('#tile3')];
+let middleRow = [$('#tile4'), $('#tile5'), $('#tile6')];
+let bottomRow = [$('#tile7'), $('#tile8'), $('#tile9')];
+let firstCol = [$('#tile1'), $('#tile4'), $('#tile7')];
+let secondCol = [$('#tile2'), $('#tile5'), $('#tile8')];
+let thirdCol = [$('#tile3'), $('#tile6'), $('#tile9')];
+let leftDiag = [$('#tile1'), $('#tile5'), $('#tile9')];
 let rightDiag = [$('#tile3'), $('#tile5'), $('#tile7')];
 
 
@@ -16,7 +16,7 @@ let rightDiag = [$('#tile3'), $('#tile5'), $('#tile7')];
 
 const checkWin = function(winArray) {
   for(let i = 0; i < winArray.length; i++) {
-    
+
 
   }
 
--- assets/scripts/game/move.js
+++ assets/scripts/game/move.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const game = require('./gameplay');
+const user = require('./users');
 
 let gameBoard = [$('#tile1'),$('#tile2'),$('#tile3'),$('#tile4'),$('#tile5'),$('#tile6'),
 $('#tile7'),$('#tile8'),$('#tile9')];
